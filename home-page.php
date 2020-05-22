bb068cf028dca4034b2921e515ad87c3feed60fe
Thanh Doan <congthanh3297@gmail.com>
2020-05-22 15:53:00 +0800

    Some changes to CSS and index.html for testing

--- home-page.php
+++ home-page.php
@@ -5,5 +5,20 @@ Template Name: Home page
 ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 
+<div id="carousel" class="carousel slide background" data-ride="carousel">
+    <h2>Title Text here</h2>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
+
 <?php get_footer();
 /* Tells WordPress to include footer.php */ ?>
