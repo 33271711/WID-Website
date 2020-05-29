cdb17c1a10ca8381720a267ea4b3dccdcbcff6d1
Thanh Doan <congthanh3297@gmail.com>
2020-05-29 12:42:11 +0800

    styling and footer

---
+++ about_us.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: Home page
+*/
+?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
 <footer class="container-fluid">
-
-</footer>
+    <div class="row">
+    <div class= "footer-content">
+          <p>CONTACT US AT<br>
+  <a href="admin@womenindentistry.net">admin@womenindentistry.net</a></p> 
+    <div class="col-md-6 text-center">
+       <img src="images/widlogo.png" alt="widlogo" width="250px">
+    </div>
+    <div class="col-md-6 text-center hover">
+        <a href="admin@womenindentistry.net"><img src="images/inlogo.png" alt="inlogo"></a>
+        <a href="admin@womenindentistry.net"><img src="images/fblogo.png" alt="fblogo"></a>
+    </div>
+    </div>
+    </div>
+    <hr width=100% align=center>
+    <div class= "footer-content">
+    <p style="color:darkgrey;">© 2020 Women In Dentistry Society. All rights reserved.
+        <a href="sitemapURL.com">Sitemap</a></p>
+    </div>    
+</footer>  
 
 <!—DELETE ALL JAVASCRIPT -->
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
