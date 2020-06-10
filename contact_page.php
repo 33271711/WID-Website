0c4cc0739c80cf5a93b082e66a16baaa20022eeb
33422994 <61783001+33422994@users.noreply.github.com>
2020-06-10 14:15:38 +0800

    Update contact_page.php

--- contact_page.php
+++ contact_page.php
@@ -8,14 +8,14 @@ Template Name: Home page
 <div id="header" class="small-header">
     <img src="" class="img"><br>
     <span class="header-mid">Contact</span><br>
-    <span class="sub-header">Info here</span>
+    <span class="sub-header">Feel free to get in touch with us if you have any questions, comments or feedback</span>
 </div>
 
 <div id="healingsmiles" class="container-fluid"></div>
     <img src="" class=""><br>
     <span class="img-overlay-right"><img src="images/home_page-carousel_overlay-white.png" alt = "bg" class="carousel-overlay"></span>
-    <span class="txt-overlay-tagline-right">Here is a title text</span>
-    <span class="txt-overlay-desc-right">Here is a description</span>
+    <span class="txt-overlay-tagline-right">Healing Smiles</span>
+    <span class="txt-overlay-desc-right">Are you interested in giving back to the community on a personal level? Join our clinical team who provide pro-bono dental care and rehabilitation to survivors of domestic violence. This initiative is supported by ADA(WA), ADHF and UWA Dental School.</span>
 
 <div id="speaker" class="container-fluid"></div>
     <img src="" class=""><br>
