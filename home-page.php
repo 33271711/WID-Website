d8e2988222a6414204b75ca597ca273527cf0b67
Thanh Doan <congthanh3297@gmail.com>
2020-06-12 00:06:31 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -38,7 +38,7 @@ Template Name: Homepage
         <span class="img-overlay"><img src="<?php echo get_template_directory_uri(); ?>/images/home_page-carousel_overlay-white.png" alt="bg" class="carousel-overlay"></span>
         <h1 class="txt-overlay-tagline"><?php the_field('title'); ?></h1>
         <span class="txt-overlay-desc"><?php the_field('tagline'); ?></span>
-    <a href='http://wuk.yew.mybluehost.me/contact-us/'><button class="btn-lg" role="button">JOIN US</button></a>
+        <a href='http://wuk.yew.mybluehost.me/contact-us/' class="btn-lg" role="button">JOIN US</a>
     </div>
 
 
