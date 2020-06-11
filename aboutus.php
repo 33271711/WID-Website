c408cf5b632e67b9cc5dbcacf422e6603609de34
Thanh Doan <congthanh3297@gmail.com>
2020-06-12 01:39:56 +0800

    a

--- aboutus.php
+++ aboutus.php
@@ -5,10 +5,9 @@ Template Name: About Us
 ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <div class="container-fluid">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/aboutus-header.jpg" alt="Header Image" class="hero-image">
+    <img src="<?php the_field('header'); ?>" alt="Header Image" class="hero-image">
   <div>
     <h1 class="hero-text Heading">About Us</h1>
-        <a href="" class="btn-lg-about-center" role="button">CONTACT US</a>
   </div>
 </div>
 
@@ -17,10 +16,10 @@ Template Name: About Us
 <div class="Title">  
 <p class="AboutTitle"> Our Values </p>
     </div>
-	<p class="AboutParagraph"> Support network for female dentists, dental specialists and dental students in Western Australia <br>
-    Assist the Western Australian community through charitable projects <br>
-	Promote mentorship, communication and connection <br> 
-	Provide continuing professional development</p>
+	<p class="AboutParagraph"> <?php the_field('value1'); ?> <br>
+    <?php the_field('value2'); ?> <br>
+	<?php the_field('value3'); ?> <br> 
+	<?php the_field('value4'); ?></p>
     </div>
     
     <!-- What do we do section -->  
@@ -29,7 +28,11 @@ Template Name: About Us
     <div class="col-md-6">
         <div class="about-divider">
         <div class="Heading" style="text-align: left; padding-left: 60px"> What do we do?</div>
-    <p class="AboutParagraph2" style="padding-left: 60px">Women in Dentistry  Society (WA) is a not-for-profit organisation, which was initially established to provide support for the growing number of female dentists and dental specialists in Western Australia, who were faced with the challenge of balancing multiple roles (i.e. as clinicians, practice owners, employees, mothers, daughters, significant others and individuals) whilst trying to maintain health and well-being in both their professional and personal lives. We have since expanded our society to provide support not only of dentistry but the wider community as well. <br><br> In terms of supporting dentistry, we aim to assist all dental professionals in achieving clinical excellence, improving business management and deriving personal satisfaction from career and family lives. We strongly believe that regular social and educational events aid in promoting continuing professional development and nurturing genuine friendships. Women in Dentistry Society (WA) has initiated a mentoring program, Cup of Coffee, to facilitate this. <br><br> In support of the wider community, part of the profits from our events are donated to various charities and organisations, including dental scholarships and research. Reaching out on a personal level to the vulnerable in the community has culminated in the launch of the Healing Smiles program, a clinical volunteering program that provides pro-bono dental care and rehabilitation to survivors of domestic violence.</p></div>
+    <p class="AboutParagraph2" style="padding-left: 60px">
+        <?php the_field('whatwedoPara1'); ?>
+        <?php the_field('whatwedoPara2'); ?>
+        <?php the_field('whatwedoPara3'); ?>
+        </div>
   </div>
           <div class="col-md-6">
          <img src="<?php echo get_template_directory_uri(); ?>/images/aboutus-whatdowedo.jpg" alt="What We Do Image" class="left-img">
@@ -48,24 +51,24 @@ Template Name: About Us
     <div class="container">
     <div class="row">
     <div class="col-md-3">
-	<img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Jacinta%20Vu%202.jpg" alt="Profile 1" class="profile">
-    <p class="memberRole">President</p>
-    <p class="memberName">Dr Jacinta Vu</p>
+	<img src="<?php the_field('memberImg1'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole1'); ?></p>
+    <p class="memberName"><?php the_field('memberName1'); ?></p>
     </div>        
     <div class="col-md-3">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Lucy%20De%20Jesus%202.jpg" alt="Profile 2" class="profile">
-    <p class="memberRole">Vice-President</p>
-    <p class="memberName">Dr Lucy De Jesus</p>
+	<img src="<?php the_field('memberImg2'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole2'); ?></p>
+    <p class="memberName"><?php the_field('memberName2'); ?></p>
     </div>
     <div class="col-md-3">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Rebecca%20Williams%202.jpg" alt="Profile 3" class="profile">
-    <p class="memberRole">Secretary</p>
-    <p class="memberName">Dr Rebecca Williams</p>     
+	<img src="<?php the_field('memberImg3'); ?>" alt="Profile 2" class="profile">
+    <p class="memberRole"><?php the_field('memberRole3'); ?></p>
+    <p class="memberName"><?php the_field('memberName3'); ?></p>   
     </div>
     <div class="col-md-3">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Jippy%20Buck%202.jpg" alt="Profile 4" class="profile"> 
-    <p class="memberRole">Treasurer</p>
-    <p class="memberName">Dr Jippy Buck</p>
+	<img src="<?php the_field('memberImg4'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole4'); ?></p>
+    <p class="memberName"><?php the_field('memberName4'); ?></p>
     </div>
     </div>
     </div>
@@ -73,19 +76,19 @@ Template Name: About Us
     <div class="container">
     <div class="row">
     <div class="col-md-4">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Jacinta%20Vu%202.jpg" alt="Profile 5" class="profile">
-    <p class="memberRole">Immediate Past President</p>
-     <p class="memberName">Dr Jacinta Vu</p>
+	<img src="<?php the_field('memberImg5'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole5'); ?></p>
+    <p class="memberName"><?php the_field('memberName5'); ?></p>
     </div>
     <div class="col-md-4">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Bernadette%20Pilkington%202.jpg" alt="Profile 6" class="profile">
-    <p class="memberRole">Cup of Coffee Coordinator</p>
-     <p class="memberName">Dr Bernadette Pilkington</p>
+	<img src="<?php the_field('memberImg6'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole6'); ?></p>
+    <p class="memberName"><?php the_field('memberName6'); ?></p>
     </div>
     <div class="col-md-4">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Dr%20Jacinta%20Vu%202.jpg" alt="Profile 7" class="profile"> 
-    <p class="memberRole">Healing Smiles Clinical Coordinator</p>
-     <p class="memberName">Dr Jacinta Vu</p>     
+	<img src="<?php the_field('memberImg7'); ?>" alt="Profile 1" class="profile">
+    <p class="memberRole"><?php the_field('memberRole7'); ?></p>
+    <p class="memberName"><?php the_field('memberName7'); ?></p>   
     </div>
     </div>
     </div>
@@ -100,30 +103,31 @@ Template Name: About Us
     </div>     
     <div class="col-md-4">
     <div class="EventTitle">    
-    <p> Executive Commitee 2018-2019</p>
+    <p> Executive Commitee <?php the_field('year1'); ?></p>
         </div>
-        <p class="memberName2">Healing Smiles Coordinator:	Dr Gosia Barley</p>
-        <p class="memberName2">Cup of Coffee Coordinator:	Dr Pantea Motearefi</p>
-    
+        <p class="memberName2"><?php the_field('ex1memberName1'); ?></p>
+        <p class="memberName2"><?php the_field('ex1memberName2'); ?></p>
+        <p class="memberName2"><?php the_field('ex1memberName3'); ?></p>
+        <p class="memberName2"><?php the_field('ex1memberName4'); ?></p>    
     </div>
     <div class="col-md-4">
     <div class="EventTitle">
-    <p> Executive Commitee 2016-2018</p>
+    <p> Executive Commitee <?php the_field('year2'); ?></p>
         </div>
-        <p class="memberName2">President:		Dr Gosia Barley</p>
-        <p class="memberName2">Secretary:		Dr Lucy De Jesus</p>
-        <p class="memberName2">Treasurer:		Dr Jacinta Vu</p>
-        <p class="memberName2">Events Coordinator:	Dr Pantea Motearefi</p>
+        <p class="memberName2"><?php the_field('ex2memberName1'); ?></p>
+        <p class="memberName2"><?php the_field('ex2memberName2'); ?></p>
+        <p class="memberName2"><?php the_field('ex2memberName3'); ?></p>
+        <p class="memberName2"><?php the_field('ex2memberName4'); ?></p>    
     </div>
     
     <div class="col-md-4">
     <div class="EventTitle">    
-    <p> Executive Commitee 2014-2016</p>
+    <p> Executive Commitee <?php the_field('year3'); ?></p>
         </div>
-        <p class="memberName2"> President: 		Dr Janice Kan</p>  
-        <p class="memberName2">Secretary: 		Dr Melanie McAlpine</p>
-        <p class="memberName2">Treasurer: 		Dr Marilyn Lobo</p>
-        <p class="memberName2">Events Coordinator: 	Dr Gosia Barley</p>       
+        <p class="memberName2"><?php the_field('ex3memberName1'); ?></p>
+        <p class="memberName2"><?php the_field('ex3memberName2'); ?></p>
+        <p class="memberName2"><?php the_field('ex3memberName3'); ?></p>
+        <p class="memberName2"><?php the_field('ex3memberName4'); ?></p>      
     </div>
     </div>
     </div>
